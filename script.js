b28105199861b07b33d58082ad73ab26e7f5971d
Rafael Machado <machadorafael27@gmail.com>
2023-06-05 21:27:54 -0300

    filter e map

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ const pokemons = [
   { nome: 'Squirtle', tipo: 'água', vida: 45 },
   { nome: 'Psyduck', tipo: 'água', vida: 25 }
 ]
+const aumentovida = (poke) => {
+  const novaVida = { ...poke, vida: 100 }
+  return novaVida;
+
+}
+
+//const vidacheia = pokemons.map(poke=>({nome:poke.nome,tipo:poke.tipo,vida:100}))   //<------- funciona
+
+const pokemonVidaCheia = pokemons.map(aumentovida);
+
+const pokemonsDeFogo = pokemons.filter((poke)=>{
+  return poke.tipo ==="fogo"
+})
+
+console.log(pokemonVidaCheia);
+console.log(pokemonsDeFogo);
